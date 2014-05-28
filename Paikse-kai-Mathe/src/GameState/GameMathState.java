479f1b6f9eda4fb2f1e278a2bd4c0bc1b9987fde
Evgenia-Almpanaki <it13120@uom.edu.gr>
2014-05-28 16:29:57 +0300

    changes math

--- Paikse-kai-Mathe/src/GameState/GameMathState.java
+++ Paikse-kai-Mathe/src/GameState/GameMathState.java
@@ -1,16 +1,34 @@
 package GameState;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import Background.Background;
+import Main.GamePanel;
 
 public class GameMathState extends GameState {
-	
+
 	private Background bg;
-	
-	
+	private BufferedImage image;
+
+
 	public GameMathState(GameStateManager gsm){
 		this.gsm = gsm;
+
+		//init background
+		bg=new Background("/math.jpg",1);
+		bg.setVector(0, 0);
+
+		//init image
+		try {
+			image=ImageIO.read(getClass().getResourceAsStream("/math_textfield.jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
@@ -27,7 +45,10 @@ public class GameMathState extends GameState {
 
 	@Override
 	public void render(Graphics2D g) {
-		// TODO Auto-generated method stub
+
+		bg.render(g);
+		g.drawImage(image, GamePanel.WIDTH/4, GamePanel.HEIGHT/4,GamePanel.WIDTH/2, GamePanel.HEIGHT/3,null);
+		
 
 	}
 
